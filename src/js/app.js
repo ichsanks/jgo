b37a75c756d8adc5126d47aa86b134c1c99bdac2
Ichsanks <ichan04@gmail.com>
2018-01-03 23:28:56 +0700

    Adding pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,37 @@
 const css = require('../scss/app.scss');
 
 console.log("It works!");
+
+$(document).ready(function() {
+	
+	$(window).scroll(function() {
+		var height = $(window).scrollTop();
+
+		if(height > 80) {
+			$('.navbar-top').addClass('scrolled');
+		} else {
+			$('.navbar-top').removeClass('scrolled');
+		}
+	})
+
+	$('.navbar-toggler').on('click', function() {
+		$(this).toggleClass('open');
+	})
+
+	$('[data-toggle="collapse"]').click(function() {
+		var target = $(this).data('target');
+		$(target).toggleClass('open');
+	});
+
+	$('.dropdown').on('click', function() {
+		$(this).toggleClass('open');
+	})
+
+	$('.accordion')
+		.on('click', '[data-toggle="accordion"]', function(e) {
+			e.preventDefault();
+			$(this).closest('.accordion-items').siblings().removeClass('open');
+			$(this).closest('.accordion-items').toggleClass('open');
+		})
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,17 @@ module.exports = {
 				use: ['style-loader', 'css-loader', 'sass-loader']
 			},
 			{
+				test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						name: '[name].[ext]',
+						outputPath: 'fonts/',    // where the fonts will go
+						publicPath: '../'       // override the default path
+					}
+				}]
+			},
+			{
 				test: /\.pug$/,
 				use: 'pug-loader'
 			}
@@ -40,10 +51,50 @@ module.exports = {
 			template: './src/login.pug',
 			filename: 'login.html'
 		}),
+		new HtmlWebpackPlugin({
+			inject: true,
+			hash: true,
+			template: './src/register.pug',
+			filename: 'register.html'
+		}),
+		new HtmlWebpackPlugin({
+			inject: true,
+			hash: true,
+			template: './src/jobs.pug',
+			filename: 'jobs.html'
+		}),
+		new HtmlWebpackPlugin({
+			inject: true,
+			hash: true,
+			template: './src/services.pug',
+			filename: 'services.html'
+		}),
+		new HtmlWebpackPlugin({
+			inject: true,
+			hash: true,
+			template: './src/new-job.pug',
+			filename: 'new-job.html'
+		}),
+		new HtmlWebpackPlugin({
+			inject: true,
+			hash: true,
+			template: './src/new-service.pug',
+			filename: 'new-service.html'
+		}),
+		new HtmlWebpackPlugin({
+			inject: true,
+			hash: true,
+			template: './src/how-it-works.pug',
+			filename: 'how-it-works.html'
+		}),
 		new ExtractTextPlugin({
 			filename: 'css/app.css',	
 			disable: true,
 			allChunks: true
+		}),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery'
 		}),
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.NamedModulesPlugin()
